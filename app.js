dbfba1f09a46c1e8a7e09368d635bd62047bca1e
agonzalez <agonzalez@fusionlabs.net>
2018-10-19 10:00:41 -0500

    basic auth example

--- app.js
+++ app.js
@@ -1,12 +1,63 @@
+const basicAuth = require("express-basic-auth");
 const express = require("express");
 const app = express();
 
-const port = 8228; 
+const authorizer = (username, password /*, callback*/) =>
+{
+	//let result = callback(null, false);
+	let result = false;
+
+	console.log("Username: %s | Password: %s", username, password)
+	
+	if (username && password)
+	{
+		if (config.credentials[username] && config.credentials[username] === password)
+		{
+			//result = callback(null, true);
+			result = true;
+
+			console.log("Authenticated: %s @ %s", username, new Date().toDateString());
+		}
+		else
+		{
+			console.log("Rejected: %s @ %s", username, new Date().toDateString());
+		}
+	}
+	return result;
+};
+
+const getUnauthorizedResponse = (request) =>
+{
+	let result = "No credentials provided.";
+
+	if (request.auth)
+	{
+		result = `The provided credentials: (${request.auth.user} - ${request.auth.password}) were rejected.`
+	}
+	return result;
+};
+
+const port = 8286;
+const config = {
+	challenge: true,
+	authorizer: authorizer,
+	credentials: {
+		"admin": "pudding",
+		"blue": "skies",
+		"green": "compartment"
+	},
+	unauthorizedResponse: getUnauthorizedResponse,
+	//authorizeAsync: true
+};
+
+app.use(basicAuth(config));
 
 app.get("/", (request, response) =>
 {
-	response.setHeader("", "");
-	reponse.send("Abril...");
+	//console.log("Request: %o", request.headers);
+
+	response.send("Abril...");
 }); //.listen(port);
 
-app.listen(port);
+app.listen(port, () => console.log("Server running @ http://localhost:%d", port));
+
